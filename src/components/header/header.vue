22006fe59b53b6cba3c4f304a99f494e32a9f875
YeaseonZhang <yeaseonzhang@gmail.com>
2017-03-09 12:42:45 +0800

    create star component

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -31,7 +31,9 @@
         </div>
         <div v-show="detailShow" class="detail">
           <div class="detail-wrapper clearfix">
-            <div class="detail-main"></div>
+            <div class="detail-main">
+              <h1 class="name">{{ seller.name }}</h1>
+            </div>
           </div>
           <div class="detail-close">
             <i class="icon-close"></i>
@@ -214,10 +216,17 @@
             overflow: auto;
             background: rgba(7, 17, 27, 0.8);
             .detail-wrapper {
+              width: 100%;
               min-height: 100%;
               .detail-main {
                 margin-top: 64px;
                 padding-bottom: 64px;
+                .name {
+                  text-align: center;
+                  line-height: 16px;
+                  font-size: 16px;
+                  font-weight: 700;
+                }
               }
             }
             .detail-close {
---
+++ src/components/star/star.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="star" :class="starType">
+    <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      size: {
+        type: Number
+      },
+      score: {
+        type: Number
+      }
+    },
+    computed: {
+      starType() {
+        return 'star-' + this.size;
+      }
+    }
+  }
+</script>
+
+<style lang="sass">
+  .star {
+    font-size: 0;
+    .star-item {
+      display: inline-block;
+      background-repeat: no-repeat;
+    }
+    &.star-48 {
+
+    }
+    &.star-36 {
+
+    }
+    &.star-24 {
+
+    }
+  }
+</style>
